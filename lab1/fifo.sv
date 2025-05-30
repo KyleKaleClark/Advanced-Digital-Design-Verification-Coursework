6324807a4e9049ee41d9af9e06ce1162a3618cb0
a21164_asu <a21164_asu@nc-asu6-l01.apporto.com>
2025-05-30 02:18:24 +0000

    almost signals are done (probably)

--- lab1/fifo.sv
+++ lab1/fifo.sv
@@ -5,13 +5,15 @@ module fifo #(parameter DSIZE=8, parameter ASIZE=4)
 	input rinc, rclk, rrst_n,
 	
 	output [DSIZE-1:0] rdata,
-	output wfull,
-	output rempty
+	output wfull, wfull_a,
+	output rempty, rempty_a
 );
 
 	logic [ASIZE-1:0] waddr, raddr;
 	logic [ASIZE:0] wptr, rptr, wq2_rptr, rq2_wptr;
 	
+	
+	//synchronizer blocks
 	sync_r2w sync_r2w (.*);
 	sync_w2r sync_w2r (.*);
 	
@@ -21,6 +23,7 @@ module fifo #(parameter DSIZE=8, parameter ASIZE=4)
 									.wclken(winc), .wfull(wfull),
 									.wclk(wclk));
 
+	//empty/full signal blocks
 	rptr_empty #(ASIZE) rptr_empty(.*);
 	wptr_full #(ASIZE) wptr_full(.*);
 
@@ -89,7 +92,7 @@ module rptr_empty #(parameter ADDRSIZE = 4)
 	input		[ADDRSIZE:0]	rq2_wptr,
 	input						rinc, rclk, rrst_n,
 	output logic [ADDRSIZE:0]	rptr,
-	output logic				rempty,
+	output logic				rempty, rempty_a,
 	output 		[ADDRSIZE-1:0]	raddr
 );
 
@@ -109,14 +112,20 @@ module rptr_empty #(parameter ADDRSIZE = 4)
 	assign rgraynext = (rbinnext>>1) ^ rbinnext;
 	
 	
-	//fifo almost empty should = wptr+4
-	assign rempty_val = (rgraynext == rq2_wptr+3'h4);
+	//empty values
+	assign rempty_val = (rgraynext == rq2_wptr);
+	
+	//almost empty = 3rd MSB = 1, while the rest are 0s. 3RD MSB is 1/4th the total size
+	assign rempty_almost_val = (rgraynext == {rq2_wptr[ADDRSIZE:ADDRSIZE-1], ~rq2_wptr[ADDRSIZE-2], rq2_wptr[ADDRSIZE-3:0]});
+	 
 	
 	always_ff @(posedge rclk or negedge rrst_n) begin
 		if (!rrst_n)
 			rempty <= 1'b1;
-		else
+		else begin
 			rempty <= rempty_val;
+			rempty_a <= rempty_almost_val;
+		end
 	end
 	
 	
@@ -127,7 +136,7 @@ module wptr_full	#(parameter ADDRSIZE = 4)
 (
 	input		[ADDRSIZE:0]		wq2_rptr,
 	input							winc, wclk, wrst_n,
-	output logic					wfull,
+	output logic					wfull, wfull_a,
 	output		[ADDRSIZE-1:0]		waddr,
 	output logic [ADDRSIZE:0]		wptr
 );
@@ -151,11 +160,17 @@ module wptr_full	#(parameter ADDRSIZE = 4)
 	//needs to be changed for almost instead of full full
 	assign wfull_val = (wgraynext == {~wq2_rptr[ADDRSIZE:ADDRSIZE-1], wq2_rptr[ADDRSIZE-2:0]});
 	
+	//almost full = wgraynext = bit[ADDRSIZE], ~bit[ADDRSIZE-1:ADDRSIZE-2], bit[ADDRSIZE-3:0]
+	//essentially 1st MSB = 0, 2nd/3rd MSB = 1, remaining bits = 0, = 3/4th of max value	
+	assign wfull_almost_val = (wgraynext == {wq2_rptr[ADDRSIZE], ~wq2_rptr[ADDRSIZE-1:ADDRSIZE-2], wq2_rptr[ADDRSIZE-3:0]});
+	
 	always_ff @(posedge wclk or negedge wrst_n) begin
 		if (!wrst_n)
 			wfull <= 1'b0;
-		else
+		else begin
 			wfull <= wfull_val;
+			wfull_a <= wfull_almost_val;			
+		end
 	end
 
 endmodule

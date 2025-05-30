1cb5783dc8e78867c70f210805f0c9c59b1d24bc
a24541_asu <a24541_asu@nc-asu6-l01.apporto.com>
2025-05-30 16:11:53 +0000

    Updated testbench to run all three tests

--- lab1/fifo_tb.sv
+++ lab1/fifo_tb.sv
@@ -10,7 +10,7 @@ integer scenario_num;
 fifo f1(.*);
 	
 	//clk logic
-	//Write clock is 2x read clock
+	//Write clock is 4x read clock
 	
 	initial begin
 		wclk=1'd0; #10;
@@ -25,8 +25,14 @@ fifo f1(.*);
 		#10 rclk = ~rclk; #10;
 		end
 	end
-
+	
+	//Set up for actual test
+	//1st test is filling the fifo up with read disabled
+	//Disables write and enables read to verify fifo empties
+	//Last test enables read and write together
+	
 	initial begin
+		//First test
 		wrst_n =1'd0;rrst_n=1'd0;wdata=8'd1; winc=1'd0; rinc=1'd0; #500;
 		wrst_n = 1'd1; rrst_n=1'd1; winc=1'd1;
 		#20;
@@ -62,9 +68,31 @@ fifo f1(.*);
 		#20;
 		wdata = 8'd2;
 		#20;
-		
+		//End of first test
+		//
+		//Start of Second test
 		winc=1'd0; rinc=1'd1; #1000;
-	
+		//End of second test
+		//
+		//Start of third test
+		wrst_n=1'd0; rrst_n=1'd0; rinc=1'd0;  #60;
+		wrst_n=1'd1; rrst_n=1'd1; #60;
+		winc=1'd1; rinc=1'd1;  wdata= 8'd1; #20;
+		wdata = 8'd2; #20;
+		wdata = 8'd3; #20;
+		wdata = 8'd4; #20;
+		wdata = 8'd5; #20;
+		wdata = 8'd6; #20;
+		wdata = 8'd7; #20;
+		wdata = 8'd8; #20;
+		wdata = 8'd9; #20;
+		wdata = 8'd10; #20;
+		wdata = 8'd11; #20;
+		wdata = 8'd12; #20;
+		wdata = 8'd13; #20;
+		wdata = 8'd14; #20;
+		wdata = 8'd15; #20;
+		//End of third test
 	end
 
 	initial begin

a1dcac8b81ea8ef0656d5777a2d1253330e97b43
a24541_asu <a24541_asu@nc-asu6-l01.apporto.com>
2025-05-30 02:21:21 +0000

    Testbench that compiles and simultates

--- lab1/fifo_tb.sv
+++ lab1/fifo_tb.sv
@@ -12,47 +12,29 @@ fifo f1(.*);
 	//clk logic
 	//Write clock is 2x read clock
 	
-	always begin
+	initial begin
+		wclk=1'd0; #10;
+		repeat (100) begin
 		#5 wclk = ~wclk; #5;
-	end
-	always begin
-		#10 rclk = ~rclk; #10;
+		end	
 	end
 
-	task fifo_full_then_empty();
-		wdata = 8'd1;
-		wrst_n = 1'd0; rrst_n = 1'd0; rinc=1'd0; #10;
-		wrst_n = 1'd1; rrst_n = 1'd1; winc=1'd1; #10
+	initial begin
+		rclk=1'd0; #10;
+		repeat (100) begin
+		#10 rclk = ~rclk; #10;
+		end
+	end
+
+	initial begin
+		wrst_n =1'd0;rrst_n=1'd0;wdata=8'd1; winc=1'd0; rinc=1'd0; #10;
+		wrst_n = 1'd1; rrst_n=1'd1; winc=1'd1; #10;
 		#100;
 		winc=1'd0; rinc=1'd1; #10;
 		#100;
-	endtask
-
-	
-	task fifo_rw();
-		winc = 1'd1; rinc = 1'd1; #10
+		winc=1'd1; #10;
 		#100;
-	endtask
-
 	
-	
-	initial begin
-		if (!$test$plusargs("test")) begin
-			$display("No test provided, exiting");
-			$finish;
-		end
-
-		if (scenario_num == 1) begin
-			fifo_full_then_empty();
-			$display("Fifo write till full and read till empty test complete");
-			$finish;
-		end
-
-		if (scenario_num ==2) begin
-			fifo_rw();
-			$display("Fifo simultaneous read and write test complete");
-			$finish;
-		end
 	end
 
 	initial begin

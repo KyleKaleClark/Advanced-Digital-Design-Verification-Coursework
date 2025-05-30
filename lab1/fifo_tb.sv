bdc11516ebd12415b088bf6640a2db0418f3de30
a24541_asu <a24541_asu@nc-asu6-l01.apporto.com>
2025-05-30 03:10:59 +0000

    updated test bench parameters

--- lab1/fifo_tb.sv
+++ lab1/fifo_tb.sv
@@ -4,7 +4,7 @@ module fifo_tb();
 logic[7:0] wdata, rdata;
 logic winc, wclk,wrst_n;
 logic rinc, rclk, rrst_n;
-logic wfull, rempty;
+logic wfull, rempty,wfull_a,rempty_a;
 integer scenario_num;
 
 fifo f1(.*);
@@ -14,26 +14,26 @@ fifo f1(.*);
 	
 	initial begin
 		wclk=1'd0; #10;
-		repeat (100) begin
+		repeat (1000) begin
 		#5 wclk = ~wclk; #5;
 		end	
 	end
 
 	initial begin
 		rclk=1'd0; #10;
-		repeat (100) begin
+		repeat (1000) begin
 		#10 rclk = ~rclk; #10;
 		end
 	end
 
 	initial begin
-		wrst_n =1'd0;rrst_n=1'd0;wdata=8'd1; winc=1'd0; rinc=1'd0; #10;
+		wrst_n =1'd0;rrst_n=1'd0;wdata=8'd1; winc=1'd0; rinc=1'd0; #50;
 		wrst_n = 1'd1; rrst_n=1'd1; winc=1'd1; #10;
-		#100;
+		#1000;
 		winc=1'd0; rinc=1'd1; #10;
-		#100;
+		#1000
 		winc=1'd1; #10;
-		#100;
+		#1000;
 	
 	end
 

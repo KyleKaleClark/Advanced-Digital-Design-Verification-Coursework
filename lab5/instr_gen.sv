b64d773c7f9fc23b55aa2c2142e85e1855ef30bc
a24541_asu <a24541_asu@nc-asu6-l01.apporto.com>
2025-06-30 03:24:32 +0000

    Completed first stage of variable randomization

--- lab5/instr_gen.sv
+++ lab5/instr_gen.sv
@@ -1,25 +1,47 @@
 
 //Add -ntb_opts uvm to the VCS command line
 
-class instruction extends uvm_instruction;
-  rand bit [] reg_a, reg_b, reg_c;
-  rand bit [] opcode;
-  rand bit [] mem_addr;
+class instruction extends uvm_transaction;
+  rand bit [4:0] reg_a, reg_b, reg_c;
+  rand bit [5:0] opcode, funct;
+  rand bit [15:0] mem_addr;
   ...
 
-  constraint unique_regs {
+  constraint unique_regs { 
+  	reg_a inside {[1:4]};
+	reg_b inside {[1:4]};
+	reg_c inside {[1:4]};
   }
 
+  			 	//Rtpe      LW          SW          BEQ
   constraint valid_opcode {
-  }
-
-  constraint illegal_opcode {
+	  	opcode inside {6'b000000, 6'b100011, 6'b101011, 6'b000100}
   }
   
-  ...
+  				//And	     Or
+  constraint valid funct {
+	  	funct inside {6'b100100, 6'b100000}
+  }
 
-  function void print_me();
-  endfunction
+  constraint valid mem {
+	  	mem_addr inside {16'd4, 16'd8, 16'd12, 16'd16}
+  }
+
+ 
+
+function void print_me();
+
+  // ----------- R-type: opcode == 0 (ADD / AND) -----------
+  if (opcode == 6'b000000) begin
+    $display("R-type reg_a=%0d reg_b=%0d  reg_c=%0d  funct=%02h", reg_a, reg_b, reg_c, funct );
+  end
+
+  // ----------- I-type: LW / SW / BEQ ---------------------
+  else begin
+    $display("I-type  opcode=%02h  reg_a=%0d  reg_b=%0d  mem_addr=%0d", opcode, reg_a, reg_b, mem_addr );
+  end
+endfunction
+
 endclass
 
 class instruction_generator;

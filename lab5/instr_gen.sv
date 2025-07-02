2a2e5bea437c8d1b1b27420465b24217cb1e8121
a24541_asu <a24541_asu@nc-asu6-l01.apporto.com>
2025-07-02 03:04:50 +0000

    Working instruction generator, still needs to be cleaned up IAW the lab manual

--- lab5/instr_gen.sv
+++ lab5/instr_gen.sv
@@ -6,8 +6,9 @@ import uvm_pkg::*;
 class instruction extends uvm_transaction;
   rand bit [4:0] reg_a, reg_b, reg_c;
   rand bit [5:0] opcode, funct;
-  rand bit [15:0] mem_addr;
- 
+  rand bit [15:0] mem_addr, branch;
+
+bit[31:0] instr; 
 
   constraint unique_regs { 
   	reg_a inside {[1:4]};
@@ -29,6 +30,10 @@ class instruction extends uvm_transaction;
 	  	mem_addr inside {16'd4, 16'd8, 16'd12, 16'd16};
   }
 
+  constraint valid_branch {
+	  	branch inside {16'd1, 16'd2, 16'd3, 16'd4};
+  }
+
 
  
 
@@ -45,52 +50,89 @@ function void print_me();
   end
 endfunction
 
+function void post_randomize();
+	if (opcode == 6'b000000) begin
+		instr = {opcode,reg_a,reg_b,reg_c,5'b00000,funct};
+	end
+
+	else if (opcode == 6'b000100) begin
+		instr = {opcode, reg_a, reg_b, branch};
+	end
+
+	else begin
+		instr = {opcode, reg_a, reg_b, mem_addr};
+	end
+endfunction
+	
 endclass
 
 class instruction_generator;
 
-  //Dynamic array to hold individual instructions	
-  instruction instr_list[];
-  bit [31:0] machine_code_list[];
-  
-  //Need to add anything here? To ensure we reach all coverage?
-  //Feels like it's too "loose" to generate all cases unless we generate a lot
-  //of instructions
-  function void generate_individual();
-	instr_list = new[15];
+//Dynamic array to hold individual instructions	
+instruction instr_list[];
+instruction pair_list[];
+bit [31:0] machine_code_list[$];
+rand bit [2:0] gap;
 
-	  for (int i = 0; i <15; i++) begin
-		  instr_list[i] = new();
-		  assert(instr_list[i].randomize());
-     		  instr_list[i].print_me();
-		  end
+  	//Constraint to determine number of instructions between pairs
+  	constraint valid_gap {
+		gap inside {4'd1,4'd2,4'd3,4'd4};
+ 	}
+ 	
+  	function void generate_individual();
+		instr_list = new[15];
 
-  endfunction
+	  	for (int i = 0; i <15; i++) begin
+			instr_list[i] = new();
+		  	assert(instr_list[i].randomize());
+     		  	instr_list[i].print_me();
+		end
 
-  //Do we need to create the txn?
-  /*
-  function instruction generate_pairs();
+  	endfunction
 
-    txn1.randomize() with { reg_a = 1; };
-    txn2.randomize() with { reg_a = 1; };
-  endfunction
+//Function to generate dependent instruction pairs
+//Also creates 1 to 4 instructions between the pair
+function instruction generate_pairs();
+	assert(randomize(gap));
+	pair_list = new[gap + 2];
+	pair_list[0] = new();
+	pair_list[gap+1] = new();
 
-  //Insert gaps of 1,2,3,4 instructions between generated pairs
-  function void insert_gaps();
-  endfunction
+    	assert(pair_list[0].randomize() with {reg_c==1;});
 
-  //Need to generate 1,2,3,4 offset for branch instructions
- 
+	for (int i = 1; i < gap+1; i++) begin
+		pair_list[i] = new();
+		assert(pair_list[i].randomize() with {reg_b != 1; reg_a != 1;});
+	end
 
-  function void generate_sequence();
-  endfunction
+	assert(pair_list[gap+1].randomize() with {reg_b==1;});
 
-  function void generate_machine_code();
-  $writememh (...);
-  endfunction
+	$display("--- generate_pairs() produced %0d additional instructions ---", gap);
+  	foreach (pair_list[i]) begin
+   	$write("[%0d] ", i);
+   	pair_list[i].print_me();
+	end
 
-  function void display_all();
-  endfunction*/
+endfunction
+   
+//Traverses both the individual and paired instruction arrays
+//Adds the 32 bit instruction to the machine_code_list
+function void generate_sequence();
+	machine_code_list = {};
+	foreach (instr_list[i])
+		machine_code_list.push_back(instr_list[i].instr);
+	foreach (pair_list[i])
+		machine_code_list.push_back(pair_list[i].instr);
+
+endfunction
+
+function void generate_machine_code();
+	$writememh ("../memfile.dat", machine_code_list);
+endfunction
+
+function void display_all();
+endfunction
+
 endclass
 
 module testbench;
@@ -99,7 +141,10 @@ module testbench;
   initial begin
     gen = new();
     gen.generate_individual();
-    //gen.display_all();
+    gen.generate_pairs();
+    gen.generate_sequence();
+    gen.generate_machine_code();
+    gen.display_all();
 
     //Copy memory from gen to the instr mem
     //OR: Call $readmemh on that file you wrote using $writememh

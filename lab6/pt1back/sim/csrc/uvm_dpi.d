adff6b721fbf195a38b91fb4c2742bff20c1e6c2
a21164_asu <a21164_asu@nc-asu6-l02.apporto.com>
2025-07-09 04:28:13 +0000

    part 1 backup

--- lab6/pt1back/sim/csrc/uvm_dpi.d
+++
@@ -1,9 +0,0 @@
-uvm_dpi.o: /usr/local2/synopsys/vcs_2022.06/etc/uvm/dpi/uvm_dpi.cc \
- /usr/local2/synopsys/vcs_2022.06/etc/uvm/dpi/uvm_regex.cc \
- /usr/local2/synopsys/vcs_2022.06/include/vpi_user.h \
- /usr/local2/synopsys/vcs_2022.06/etc/uvm/dpi/uvm_hdl.c \
- /usr/local2/synopsys/vcs_2022.06/include/sv_vpi_user.h \
- /usr/local2/synopsys/vcs_2022.06/include/vpi_user.h \
- /usr/local2/synopsys/vcs_2022.06/include/veriuser.h \
- /usr/local2/synopsys/vcs_2022.06/include/svdpi.h \
- /usr/local2/synopsys/vcs_2022.06/etc/uvm/dpi/uvm_svcmd_dpi.c
--- lab6/pt1back/sim/csrc/uvm_verdi_dpi.d
+++
@@ -1,8 +0,0 @@
-uvm_verdi_dpi.o: \
- /usr/local2/synopsys/vcs_2022.06/etc/uvm/verdi/dpi/uvm_verdi_dpi.cpp \
- /usr/local2/synopsys/vcs_2022.06/include/vpi_user.h \
- /usr/local2/synopsys/vcs_2022.06/include/veriuser.h \
- /usr/local2/synopsys/vcs_2022.06/include/sv_vpi_user.h \
- /usr/local2/synopsys/vcs_2022.06/include/vpi_user.h \
- /usr/local2/synopsys/vcs_2022.06/include/vcs_vpi_user.h \
- /usr/local2/synopsys/vcs_2022.06/include/sv_vpi_user.h
